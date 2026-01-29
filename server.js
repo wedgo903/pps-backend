ca48c9cd0e0f82f3c5bd293961269851f632f0a2
Maciej Wolanin <m.wolanin@astechnik.pl>
2026-01-29 09:32:55 +0100

    tests panel + pressure + 45min + pdf

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/version', (req, res) => {
   res.send('PPS BACKEND VERSION 13');
